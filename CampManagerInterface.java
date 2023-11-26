c7e254cabe4ed0311dc175d20ac2e22dcff9b291
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2023-11-27 00:29:04 +0800

    Delete CampManagerInterface.java

--- CampManagerInterface.java
+++
@@ -1,6 +0,0 @@
-public interface CampManagerInterface
-{
-    public void createCamp(); //Responsible for Creating Camps
-    public void editCamp(Camp camp); //Responsible for Editing Existing Camps
-    public void deleteCamp(Camp camp); //Responsible for Deleting Camps
-}
