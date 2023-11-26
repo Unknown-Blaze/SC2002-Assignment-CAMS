859e3aa031a0adbf14f7ff8154467e8f3d8966e4
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2023-11-27 01:27:47 +0800

    Update DateConverter.java

--- src/DateConverter.java
+++ src/DateConverter.java
@@ -19,14 +19,13 @@ public class DateConverter {
                 .appendOptional(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
                 .appendOptional(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
                 .appendOptional(DateTimeFormatter.ofPattern("MM.dd.yyyy"));
-                // Add more patterns as needed
+                // Can add more patterns
 
         try {
             LocalDate ldate = LocalDate.parse(s, formatterBuilder.toFormatter());
             return Date.from(ldate.atStartOfDay(ZoneId.systemDefault()).toInstant());
         } catch (DateTimeParseException e) {
             throw new ParseException("Unable to parse the date: " + s, 0);
-            // You can log the exception or handle it in some other way based on your requirements
         }
     }
 }
